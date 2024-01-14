1351282c818389566855df7183a79d1e6e038efe
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-01-14 12:54:06 +0100

    refactor: simplify logic

--- scripts/select-read-later.js
+++ scripts/select-read-later.js
@@ -2,6 +2,7 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
 
 /** @param {string} path */
 function readFile(path) {
@@ -15,31 +16,27 @@ function readFile(path) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	/** @type {AlfredItem[]} */
+	let lineNo = 0;
+
 	const readLaterItems = readFile($.getenv("read_later_file"))
 		.trim()
 		.split("\n")
-		.filter((line) => line.startsWith("- [ ] "))
 		.map((line) => {
-			const valid = line.includes("](");
-			if (!valid) {
-				return {
-					title: "Line does not have valid markdown task syntax:",
-					valid: false,
-					subtitle: line,
-				};
-			}
+			lineNo++;
+			const validItem = line.startsWith("- [ ] ") && line.includes("](");
+			if (!validItem) return {};
+
 			const title = line.split("](")[0].slice(7);
 			const url = line.split("](")[1].slice(0, -1);
 			return {
 				title: title,
 				subtitle: url,
-				arg: url,
+				arg: lineNo,
 			};
 		});
 
 	// "Add current browser tab" item
-	readLaterItems.unshift({ title: "🔖 Add current browser tab", arg: "add" });
+	readLaterItems.unshift({ title: "🔖 Add current browser tab", arg: -1, subtitle: "" });
 
 	return JSON.stringify({ items: readLaterItems });
 }
